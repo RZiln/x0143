b995f37e25ddf64129a457c6af2a54e0d7166962
RZiln <34578371+RZiln@users.noreply.github.com>
2024-07-31 19:45:49 +0300

    Update hwids.lua

--- hwids.lua
+++ hwids.lua
@@ -11,7 +11,7 @@ hwids = {
     ["56a24a2e9eb7dadfc528eeb48d69e9d5"] = true, --6/12/23
     ["3d63b711889075b7f5db6ce05bb92f85"] = true, -- fusion
     ["3ca506ab53a76a0a8a7a43764f832a30"] = true,
-    ["84792a25dd0852c74d9a50d68b33a1884544762b0751758344c61bf59a37abc8"] = true, -- 7/19
+    ["debacfe631bdd10ae799a07c40200c42d0cfbfcfe5f27294d153fbb844d26ddd"] = true, -- 7/19
     ["D8903A045BDCC8B86C8845D409463AB088BC"] = true,
     ['380466708ef8a17cdc095cdca6eee4562791877bc5d85341b84bdd2ff06a366a'] = true
 }
